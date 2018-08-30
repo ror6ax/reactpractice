d1a3036f5ecaf69a33b4882008cc603f13005da0
Gregory Reshetniak <ror6ax@gmail.com>
2018-08-30 18:23:03 +0200

    full twitter message, static

--- static-tweet/src/index.js
+++ static-tweet/src/index.js
@@ -2,13 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const Time = () => (
+  <span className='time'>3 hours ago</span>
+)
+
+const ReplyButton = () => (
+  <i className="fa fa-reply reply-button"/>
+);
+
+const RetweetButton = () => (
+  <i className="fa fa-retweet retweet-button"/>
+);
+
+const LikeButton = () => (
+  <i className="fa fa-heart like-button"/>
+);
+
+const MoreOptionsButton = () => (
+  <i className="fa fa-ellipsis-h more-options-button"/>
+);
+
 function Tweet () {
   return (
     <div className='tweet'>
       <Avatar/>
       <div className="content">
-        <NameWithHandle/>
+        <NameWithHandle/><Time/>
         <Message/>
+        <div className='buttons'>
+          <ReplyButton/>
+          <RetweetButton/>
+          <LikeButton/>
+          <MoreOptionsButton/>
+        </div>
       </div>
     </div>
   )
